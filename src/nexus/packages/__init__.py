737ea4394a0335af55f6437cbe4b75229026af95
Abdullah Enes Gülerli <excaliburman21@gmail.com>
2026-09-16 22:11:50 +0300

    feat: export package update planning API

--- src/nexus/packages/__init__.py
+++ src/nexus/packages/__init__.py
@@ -1,5 +1,12 @@
 """Linux package manager inspection and safe update planning."""
 
+from nexus.packages.actions import PackageUpdateProposal, plan_package_updates
 from nexus.packages.pacman import PackageManagerError, PackageUpdate, inspect_updates
 
-__all__ = ["PackageManagerError", "PackageUpdate", "inspect_updates"]
+__all__ = [
+    "PackageManagerError",
+    "PackageUpdate",
+    "PackageUpdateProposal",
+    "inspect_updates",
+    "plan_package_updates",
+]
